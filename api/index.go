a0ef30d0dc57b0acfffa0990f39c674c5568cda3
j178 <10510431+j178@users.noreply.github.com>
2023-03-29 17:11:21 +0800

    use single file

--- api/index.go
+++ api/index.go
@@ -5,9 +5,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
-	github_stargazer "github.com/j178/github-stargazer"
+	"github.com/nikoksr/notify"
+	"github.com/nikoksr/notify/service/bark"
+	"github.com/nikoksr/notify/service/discord"
+	"github.com/nikoksr/notify/service/telegram"
 )
 
 type StarEvent struct {
@@ -47,11 +52,58 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	)
 	ctx, cancel := context.WithTimeout(r.Context(), 3*time.Second)
 	defer cancel()
-	err = github_stargazer.Notify(ctx, title, text)
+	err = Notify(ctx, title, text)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte("OK"))
+}
+
+func envOrDefault(key, defaultValue string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return defaultValue
+}
+
+func env(key string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	panic(fmt.Sprintf("environment variable %s is not set", key))
+}
+
+func Notify(ctx context.Context, subject, message string) (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = fmt.Errorf("notify: %v", e)
+		}
+	}()
+
+	notifier := notify.New()
+	service := envOrDefault("NOTIFY_SERVICE", "bark")
+	switch service {
+	case "bark":
+		notifier.UseServices(bark.NewWithServers(env("BARK_KEY"), envOrDefault("BARK_SERVER", bark.DefaultServerURL)))
+	case "telegram":
+		tg, err := telegram.New(env("TELEGRAM_TOKEN"))
+		if err != nil {
+			return err
+		}
+		tg.SetParseMode("MarkdownV2")
+		chatIDStr := env("TELEGRAM_CHAT_ID")
+		chatID, err := strconv.ParseInt(chatIDStr, 10, 64)
+		if err != nil {
+			return err
+		}
+		tg.AddReceivers(chatID)
+		notifier.UseServices(tg)
+	case "discord":
+		ds := discord.New()
+		ds.AddReceivers(env("DISCORD_CHANNEL_ID"))
+		notifier.UseServices(ds)
+	}
+	return notifier.Send(ctx, subject, message)
 }
