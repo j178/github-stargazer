c50982d97a0da7c6b9007300fe224af5a6ce81e1
j178 <10510431+j178@users.noreply.github.com>
2023-06-28 20:51:51 +0800

    add /authorize

--- api/authorize.go
+++ api/authorize.go
@@ -1,3 +1,20 @@
 package api
 
+import (
+	"net/http"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+)
+
 // 提供一个配置的入口
+
+func Authorize(w http.ResponseWriter, r *http.Request) {
+	cfg := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     github.Endpoint,
+	}
+	url := cfg.AuthCodeURL("state")
+	http.Redirect(w, r, url, http.StatusFound)
+}
