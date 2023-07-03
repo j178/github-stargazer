70ae7a0ec2b70d695ec3aab6cdc2d97f0dc56cf9
j178 <10510431+j178@users.noreply.github.com>
2023-07-03 16:22:23 +0800

    remove github.com/nikoksr/notify

--- backend/notify/notify.go
+++ backend/notify/notify.go
@@ -1,21 +1,53 @@
 package notify
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/nikoksr/notify"
+	"github.com/sourcegraph/conc/pool"
 )
 
 // 参考 github.com/nikoksr/notify, github.com/goreleaser/goreleaser 和 github.com/megaease/easeprobe
 
 type Notifier interface {
-	notify.Notifier
 	Name() string
 	Configure(settings map[string]string) error
+	Send(context.Context, string, string) error
 }
 
-func GetNotifier(settings []map[string]string) (*notify.Notify, error) {
-	notifier := notify.New()
+type Notify struct {
+	notifiers []Notifier
+}
+
+func (n *Notify) AddNotifier(notifier Notifier) {
+	n.notifiers = append(n.notifiers, notifier)
+}
+
+func (n *Notify) Send(ctx context.Context, subject, message string) error {
+	wg := pool.New().WithContext(ctx).WithMaxGoroutines(10)
+	for _, notifier := range n.notifiers {
+		if notifier == nil {
+			continue
+		}
+
+		notifier := notifier
+		wg.Go(
+			func(ctx context.Context) error {
+				return notifier.Send(ctx, subject, message)
+			},
+		)
+	}
+
+	err := wg.Wait()
+	if err != nil {
+		return fmt.Errorf("send notify: %w", err)
+	}
+
+	return nil
+}
+
+func GetNotifier(settings []map[string]string) (*Notify, error) {
+	notify := &Notify{}
 	for _, setting := range settings {
 		serviceName := setting["service"]
 		var service Notifier
@@ -37,8 +69,8 @@ func GetNotifier(settings []map[string]string) (*notify.Notify, error) {
 		if err != nil {
 			return nil, fmt.Errorf("%s: %w", service.Name(), err)
 		}
-		notifier.UseServices(service)
+		notify.AddNotifier(service)
 	}
 
-	return notifier, nil
+	return notify, nil
 }
--- go.mod
+++ go.mod
@@ -49,7 +49,6 @@ require (
 	golang.org/x/crypto v0.10.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/net v0.11.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
